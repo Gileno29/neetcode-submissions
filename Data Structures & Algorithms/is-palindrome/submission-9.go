014b986e0387632af83f9686b8c77fedcd382023
Gileno Cordeiro Duarte <38367700+Gileno29@users.noreply.github.com>
2026-05-18 18:57:54 -0300

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.go
@@ -0,0 +1,22 @@
+func isPalindrome(s string) bool {
+	s=strings.ReplaceAll(s,"?","")
+	s=strings.ToLower(s)
+	s=strings.ReplaceAll(s," ","")
+	s=strings.ReplaceAll(s,"'","")
+	s=strings.ReplaceAll(s,",","")
+	s=strings.ReplaceAll(s,".","")
+	s=strings.ReplaceAll(s,":","")
+	s=strings.ReplaceAll(s,"!","")
+	s=strings.ReplaceAll(s,"^","")
+	fmt.Println(s)
+	for i:=0;i<len(s)/2; i++{
+		
+		if s[i]!=s[len(s)-i-1]{
+			return false
+		}
+
+	}
+
+	return true
+
+}
